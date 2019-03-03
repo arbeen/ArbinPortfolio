7b1e4fdeb736f7c87c3de1825277ff9392538484
arbeen <arbinshrestha304@gmail.com>
2019-03-03 22:01:17 +0545

    basic working site designing and content rem.

--- script.js
+++ script.js
@@ -1,5 +1,52 @@
 $(document).ready(function(){
+	$("#qualifications-content").hide();
+	$("#education-content").hide();
+	$("#interest-content").hide();
+	$("#contact-content").hide();
+
 	$("#navHeader").click(function(){
-		$("#contentHeader").fadeToggle();
-});
+		$("#qualifications-content").hide();	
+		$("#education-content").hide();
+		$("#interest-content").hide();
+		$("#contact-content").hide();
+
+		$("#home-content").fadeIn();	
+	});
+
+	$("#qualifications").click(function(){
+		$("#home-content").hide();	
+		$("#education-content").hide();
+		$("#interest-content").hide();
+		$("#contact-content").hide();
+
+		$("#qualifications-content").fadeIn();	
+	});
+
+	$("#education").click(function(){
+		$("#home-content").hide();	
+		$("#qualifications-content").hide();
+		$("#interest-content").hide();
+		$("#contact-content").hide();
+		
+		$("#education-content").fadeIn();	
+	});
+
+	$("#interest").click(function(){
+		$("#home-content").hide();	
+		$("#education-content").hide();
+		$("#qualifications-content").hide();
+		$("#contact-content").hide();
+
+		$("#interest-content").fadeIn();	
+	});
+
+	$("#contact").click(function(){
+		$("#home-content").hide();	
+		$("#qualifications-content").hide();
+		$("#interest-content").hide();
+		$("#education-content").hide();
+		
+		$("#contact-content").fadeIn();	
+	});
+
 });
